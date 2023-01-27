78daad361d36703ab4e70e04b7b1a13001b44f38
Alexmdz77 <alext77165@gmail.com>
2023-01-27 12:14:58 +0100

    ✨ Add Bonus Invites & vanity invites

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ module.exports = class extends EventEmitter {
 
             const invitesBefore = invitesCount[guild.id];
             const invitesAfter = await fetchInvites(guild);
+            let isVanity = true;
 
             for (const inviter in invitesAfter) {
                 if (invitesAfter[inviter] - invitesBefore[inviter] === 1) {
@@ -48,22 +49,69 @@ module.exports = class extends EventEmitter {
                     member.inviter = user;
                     let getData = await new Promise(async (resolve) => {
                         let userData = await keyv.get(`invitestracker_${guild.id}_${inviter}`);
-                        if (!userData) {
+                        if (!userData || !userData.invites) {
                             userData = {
                                 guildId: guild.id,
                                 userId: inviter,
-                                invites: 0
+                                invites: {
+                                    regular: 0,
+                                    bonus: 0,
+                                    leaves: 0,
+                                    fake: 0,
+                                    total: 0
+                                }
                             };
                         };
-                        userData.invites++;
+                        userData.invites = {
+                            regular: userData.invites.regular + 1,
+                            bonus: userData.invites.bonus,
+                            leaves: userData.invites.leaves,
+                            fake: userData.invites.fake,
+                            total: userData.invites.total + 1
+                        }
                         await keyv.set(`invitestracker_${guild.id}_${inviter}`, userData);
                         resolve(userData);
                     });
                     member.invites = getData.invites;
                     invitesCount[guild.id] = invitesAfter;
+                    isVanity = false;
                     return this.emit('guildMemberAdd', member);
-                };
+                }
             };
+            if (!isVanity) return;
+            member.inviter = 'vanity';
+            let data = {
+                guildId: guild.id,
+                userId: member.id,
+                invitedBy: 'vanity'
+            };
+            await keyv.set(`invitestracker_${guild.id}_${member.id}`, data);
+            let getData = await new Promise(async (resolve) => {
+                let userData = await keyv.get(`invitestracker_${guild.id}_${member.id}`);
+                if (!userData || !userData.invites) {
+                    userData = {
+                        guildId: guild.id,
+                        userId: member.id,
+                        invites: {
+                            regular: 0,
+                            bonus: 0,
+                            leaves: 0,
+                            fake: 0,
+                            total: 0
+                        }
+                    };
+                };
+                userData.invites = {
+                    regular: userData.invites.regular,
+                    bonus: userData.invites.bonus,
+                    leaves: userData.invites.leaves,
+                    fake: userData.invites.fake,
+                    total: userData.invites.total
+                }
+                await keyv.set(`invitestracker_${guild.id}_${member.id}`, userData);
+                resolve(userData);
+            });
+            member.invites = getData.invites;
             return this.emit('guildMemberAdd', member);
         });
 
@@ -72,7 +120,25 @@ module.exports = class extends EventEmitter {
             let data = await keyv.get(`invitestracker_${guild.id}_${member.id}`);
             if (!data) return;
             let userData = await keyv.get(`invitestracker_${guild.id}_${data.invitedBy}`);
-            userData ? userData.invites-- : userData = {guildId: guild.id, userId: data.invitedBy, invites: 0};
+            if (userData && userData.invites) {
+                userData.invites = {
+                    regular: userData.invites.regular,
+                    bonus: userData.invites.bonus,
+                    leaves: userData.invites.leaves + 1,
+                    fake: userData.invites.fake,
+                    total: userData.invites.total == 0 ? 0 : userData.invites.total - 1
+                }
+            } else userData = {
+                guildId: guild.id,
+                userId: data.invitedBy,
+                invites: {
+                    regular: 0,
+                    bonus: 0,
+                    leaves: 1,
+                    fake: 0,
+                    total: 0
+                }
+            };
             await keyv.set(`invitestracker_${guild.id}_${data.invitedBy}`, userData);
             keyv.delete(`invitestracker_${guild.id}_${member.id}`);
         });
@@ -88,12 +154,62 @@ module.exports = class extends EventEmitter {
             if (!guild) throw new Error('Please pass the guild');
             const users = keyv.all().filter(element => element.startsWith(`invitestracker_${guild.id}`))
             if (!users || users.length == 0) return 0;
-            let total = 0;
-            for (const user of users) {
-                total += user.invites;
-            }
-            return total;
+            return users;
         };
+
+        this.addBonusInvite = async function(user, guild, amount) {
+            if (!user || !guild) throw new Error('Please pass the user');
+            let userData = await keyv.get(`invitestracker_${guild.id}_${user.id}`);
+            if (!userData || !userData.invites){
+                userData = {
+                    guildId: guild.id,
+                    userId: user.id,
+                    invites: {
+                        regular: 0,
+                        bonus: 0,
+                        leaves: 0,
+                        fake: 0,
+                        total: 0
+                    }
+                };
+            }
+            userData.invites = {
+                regular: userData.invites.regular,
+                bonus: userData.invites.bonus + amount,
+                leaves: userData.invites.leaves,
+                fake: userData.invites.fake,
+                total: userData.invites.total + amount
+            }
+            await keyv.set(`invitestracker_${guild.id}_${user.id}`, userData);
+            return userData.invites;
+        }
+
+        this.removeBonusInvite = async function(user, guild, amount) {
+            if (!user || !guild) throw new Error('Please pass the user');
+            let userData = await keyv.get(`invitestracker_${guild.id}_${user.id}`);
+            if (!userData || !userData.invites){
+                userData = {
+                    guildId: guild.id,
+                    userId: user.id,
+                    invites: {
+                        regular: 0,
+                        bonus: 0,
+                        leaves: 0,
+                        fake: 0,
+                        total: 0
+                    }
+                };
+            }
+            userData.invites = {
+                regular: userData.invites.regular,
+                bonus: userData.invites.bonus - amount,
+                leaves: userData.invites.leaves,
+                fake: userData.invites.fake,
+                total: userData.invites.total - amount < 0 ? 0 : userData.invites.total - amount
+            }
+            await keyv.set(`invitestracker_${guild.id}_${user.id}`, userData);
+            return userData.invites;
+        }
     };
 
 };
